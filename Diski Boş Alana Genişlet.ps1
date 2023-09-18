9b2b5a15de60cab0d6e15026bfb67ca655fc5c81
stan0ne <savas_boluk86@hotmail.com>
2023-09-18 10:38:12 +0300

    Tek partitionlu sistemlerde diski boş alana hızlıca genişletir

---
+++ Diski Boş Alana Genişlet.ps1
@@ -0,0 +1,24 @@
+﻿function List-Disks {
+'list disk' | diskpart |
+? { $_ -match 'disk (\d+)\s+online\s+\d+ .?b\s+\d+ [gm]b' } |
+% { $matches[1] } 
+Update-Disk -Number $matches[1]
+}
+
+function List-Partitions($disk) {
+"select disk $disk", "list partition" | diskpart |
+? { $_ -match 'partition (\d+)' } |
+% { $matches[1] }
+}
+
+function Extend-Partition($disk, $part) {
+"select disk $disk","select partition $part","extend" | diskpart | 
+Out-Null
+}
+
+List-Disks | % {
+$disk = $_
+List-Partitions $disk | % {
+Extend-Partition $disk $_
+}
+}
