e8a82b4a6085da27d28a1e7be6b0657cd37f4a78
stan0ne <savas_boluk86@hotmail.com>
2023-09-18 10:34:40 +0300

    Bilgisayarda oturum açmış olan domain kulllanıcısını bul

---
+++ BilgisayardaOturumAcanHesapBul.ps1
@@ -0,0 +1,15 @@
+﻿cls
+$select = "E"
+
+do
+{
+$computer=read-host "`nBilgisayar adı / IP Girin" 
+if(Test-Connection -ComputerName $computer -Quiet -Count 2 -Delay 1)
+{
+Get-CimInstance -ClassName Win32_computersystem -comp $computer | select Username,Caption,Manufacturer,Model | out-host
+#Get-CimInstance -ClassName Win32_computersystem -comp $computer | select Username,Caption,Manufacturer,Model | out-host
+} 
+else { Write-Host "$computer makinesine ulaşılamıyor" -ForegroundColor Red | Out-Host }
+$select = Read-Host "Başka bilgisayar sorgulama için E basın"
+$select
+}while ($select -eq "E")
