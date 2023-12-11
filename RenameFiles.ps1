179f94bcde91ae64ec3a4fc3bd7b4ff74381a2f4
stan0ne <savas_boluk86@hotmail.com>
2023-12-11 14:49:58 +0300

    Toplu Dosya Yeniden Adlandırma

--- RenameFiles.ps1
+++ RenameFiles.ps1
@@ -1,2 +1,8 @@
-# Klasördeki tüm dosyaların ilk 13 harfini siler
- get-childitem *.* | rename-item -newname { [string]($_.name).substring(13) }
+# Klasördeki tüm dosyaların ilk 3 karakterini siler
+ get-childitem *.* | rename-item -newname { [string]($_.name).substring(3) }
+
+# Klasördeki tüm dosyaların son 3 karakterini siler
+Get-ChildItem * | rename-item -newname { $_.name.substring(0,$_.name.Length-3) }
+
+# Klasördeki tüm dosyaların son 3 karakteri haricindeki karakterleri siler
+Get-ChildItem * | rename-item -newname { $_.name.substring($_.name.Length-3) }
